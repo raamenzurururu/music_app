17c8907f92690fedc3bd084c9b87376765ad9bf9
github raamenzurururu <reku9696@gmail.com>
2020-07-12 02:31:35 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <no-ssr>
+      <Music />
+  </no-ssr>
+</template>
+
+<script>
+import Music from '~/components/Music'
+
+export default {
+    components: {
+        Music
+    }
+}
+</script>
+
+<style>
+
+</style>
